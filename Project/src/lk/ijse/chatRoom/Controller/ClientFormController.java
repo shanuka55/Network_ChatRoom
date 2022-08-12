e28ee5d90219efe4c11e54180bdaa421ccd8dc79
shanuka55 <shanukalakshan5555@gmail.com>
2022-08-12 21:17:26 +0530

    content Updated

--- Project/src/lk/ijse/chatRoom/Controller/ClientFormController.java
+++ Project/src/lk/ijse/chatRoom/Controller/ClientFormController.java
@@ -1,15 +1,28 @@
 package lk.ijse.chatRoom.Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class ClientFormController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientFormController implements Initializable {
     public Label clientName;
     public TextArea textArea;
     public TextField textFieldSent;
 
+    String Name;
+
     public void sentBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Name = LoginFormController.clientName;
+        clientName.setText(Name);
+        System.out.println(Name);
     }
 }
--- Project/src/lk/ijse/chatRoom/Controller/LoginFormController.java
+++ Project/src/lk/ijse/chatRoom/Controller/LoginFormController.java
@@ -1,11 +1,37 @@
 package lk.ijse.chatRoom.Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-public class LoginFormController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginFormController implements Initializable {
     public TextField nameTextfield;
 
-    public void startBtnOnAction(ActionEvent actionEvent) {
+    public static String clientName;
+
+    public void startBtnOnAction(ActionEvent actionEvent) throws IOException {
+
+        clientName = nameTextfield.getText();
+
+        Parent parent = FXMLLoader.load(getClass().getResource("../view/ClientForm.fxml"));
+        Stage stage = new Stage();
+        stage.setScene(new Scene(parent));
+        stage.setResizable(false);
+        stage.show();
+
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
     }
 }
