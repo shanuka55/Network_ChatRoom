e66c64cb8fbf3d24ec22acda6d542d18ef2f4904
shanuka55 <shanukalakshan5555@gmail.com>
2022-08-12 20:28:45 +0530

    pakageCreated

---
+++ Project/src/lk/ijse/chatRoom/Controller/ClientFormController.java
@@ -0,0 +1,4 @@
+package lk.ijse.chatRoom.Controller;
+
+public class ClientFormController {
+}
---
+++ Project/src/lk/ijse/chatRoom/Controller/LoginFormController.java
@@ -0,0 +1,13 @@
+package lk.ijse.chatRoom.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class LoginFormController {
+
+
+    public TextField nameTextfield;
+
+    public void startBtnOnAction(ActionEvent actionEvent) {
+    }
+}
