2e6a86d3eb86e8da4de3824c637cd414c3c6210d
shanuka55 <shanukalakshan5555@gmail.com>
2022-08-15 21:45:09 +0530

    Contend updated....

--- Project/src/lk/ijse/chatRoom/Controller/ClientFormController.java
+++ Project/src/lk/ijse/chatRoom/Controller/ClientFormController.java
@@ -6,6 +6,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -16,7 +20,27 @@ public class ClientFormController implements Initializable {
 
     String Name;
 
-    public void sentBtnOnAction(ActionEvent actionEvent) {
+    final int port = 5000;
+    Socket socket;
+
+    String sms;
+
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
+
+    public void sentBtnOnAction(ActionEvent actionEvent) throws IOException {
+
+        sendMsg();
+    }
+
+    private void sendMsg() throws IOException {
+        dataInputStream = new DataInputStream(socket.getInputStream());
+        dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+        dataOutputStream.writeUTF(textFieldSent.getText().trim());
+        dataOutputStream.flush();
+        textFieldSent.clear();
+
     }
 
     @Override
@@ -24,5 +48,22 @@ public class ClientFormController implements Initializable {
         Name = LoginFormController.clientName;
         clientName.setText(Name);
         System.out.println(Name);
+
+        new Thread(() -> {
+            try {
+                socket = new Socket("localhost",port);
+                System.out.println("Client  started....!");
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+                while (socket.isConnected()){
+                    sms = dataInputStream.readUTF();
+                    textArea.appendText("\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
     }
 }
--- Project/src/lk/ijse/chatRoom/Controller/LoginFormController.java
+++ Project/src/lk/ijse/chatRoom/Controller/LoginFormController.java
@@ -8,17 +8,39 @@ import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginFormController implements Initializable {
+
     public TextField nameTextfield;
 
     public static String clientName;
 
+    ServerSocket serverSocket;
+    Socket socket;
+
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
+
+    public static String sms;
+
+
     public void startBtnOnAction(ActionEvent actionEvent) throws IOException {
 
+        msgSender();
+
+
+    }
+
+    private void msgSender() throws IOException {
+
+        //text feeld clientName Assign to this variable--->
         clientName = nameTextfield.getText();
 
         Parent parent = FXMLLoader.load(getClass().getResource("../view/ClientForm.fxml"));
@@ -27,11 +49,39 @@ public class LoginFormController implements Initializable {
         stage.setResizable(false);
         stage.show();
 
+        new Thread(() -> {
+            try {
+                socket = serverSocket.accept();
+                System.out.println("Client connected....!");
+
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+                while (!socket.isConnected()){
+                    sms = dataInputStream.readUTF();
+                    System.out.println(sms);
+                    dataOutputStream.writeUTF(sms.trim());
+                    dataOutputStream.flush();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
 
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        new Thread(() -> {
+            try {
+                serverSocket = new ServerSocket(5000);
+                System.out.println("Server Start....!");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
 
     }
 }
